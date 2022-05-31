8ce59b91b023a6f59553b4222be12348e9d5f76a
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-05-31 20:37:43 +0700

    [FIX] Deploy to vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
 export default {
    // Global page headers: https://go.nuxtjs.dev/config-head
+   target: "static",
+   ssr: false,
+   generate: {
+      fallback: true,
+   },
+
    head: {
       title: "nuxt-movie",
       htmlAttrs: {
