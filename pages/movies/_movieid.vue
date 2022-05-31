17172a42cded172b07c9f4bf69473c9dd636e674
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-05-31 12:59:19 +0700

    [ADD] Movie Details

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -16,52 +16,48 @@
          ></span>
       </div>
 
-      <div
-         class="container absolute"
-      >
-        
-        <div class="w-full md:w-[70%] mx-auto grid grid-cols-5 justify-center items-end gap-4 md:gap-8 -mt-12 z-10 inset-x-0 " >
+      <div class="container absolute">
+         <div
+            class="w-full md:w-[70%] mx-auto grid grid-cols-5 justify-center items-end gap-4 md:gap-8 -mt-12 z-10 inset-x-0"
+         >
             <img
-            :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
-            alt=""
-            class="col-span-2 w-48 md:w-56 lg:w-60 mx-auto rounded-xl"
-         />
+               :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+               alt=""
+               class="col-span-2 w-48 md:w-56 lg:w-60 mx-auto rounded-xl"
+            />
 
-         <div class="col-span-3 pt-8 pb-4">
-            <h1 class="movie_detail-title">
-               {{ movie.title }}
-            </h1>
+            <div class="col-span-3 pt-8 pb-4">
+               <h1 class="movie_detail-title">
+                  {{ movie.title }}
+               </h1>
 
-            <p
-               class="px-2 py-[2px] text-xs my-2 bg-red-600 text-white rounded-md inline-block"
-            >
-               {{ movie.vote_average }}
-            </p>
-            <p class="text-gray-400 text-xs">{{ movie.tagline }}</p>
-            <p class="text-gray-400 mt-1 text-xs">
-               {{
-                  new Date(movie.release_date).toLocaleString("en-us", {
-                     month: "long",
-                     day: "numeric",
-                     year: "numeric",
-                  })
-               }}
-            </p>
-            <p class="text-white text-base hidden md:block">
-               {{ movie.overview }}
-            </p>
+               <p
+                  class="px-2 py-[2px] text-xs my-2 bg-red-600 text-white rounded-md inline-block"
+               >
+                  {{ movie.vote_average }}
+               </p>
+               <p class="text-gray-400 text-xs">{{ movie.tagline }}</p>
+               <p class="text-gray-400 mt-1 text-xs">
+                  {{
+                     new Date(movie.release_date).toLocaleString("en-us", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                     })
+                  }}
+               </p>
+               <p class="text-white text-base hidden md:block">
+                  {{ movie.overview }}
+               </p>
+            </div>
          </div>
-        
-        </div>
-         <p class="text-xl tracking-wider text-white font-semibold mt-6">Overview</p>
-         <p class="text-white text-sm  mt-1 leading-relaxed">
-               {{ movie.overview }}
-            </p>
-    
-        
+         <p class="text-xl tracking-wider text-white font-semibold mt-6">
+            Overview
+         </p>
+         <p class="text-white text-sm mt-1 leading-relaxed">
+            {{ movie.overview }}
+         </p>
       </div>
-
-        
    </div>
 </template>
 
